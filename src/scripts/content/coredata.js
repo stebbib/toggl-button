7124d806412a630eefafd77101a7d32b134e8fed
Stefan Brynjólfsson <starkadur@starkadur.gv.local>
2015-05-26 16:12:51 +0000

    Now works on both ui dialog and non ui dialog

--- src/scripts/content/coredata.js
+++ src/scripts/content/coredata.js
@@ -5,7 +5,21 @@ togglbutton.render('.ui-dialog:not(.toggl)', {observe: true}, function (elem) {
   var link,
     description = document.getElementsByClassName("value")[0].textContent,
     project = $('#pagetitle a').textContent;
-    console.log(project);
+
+  link = togglbutton.createTimerLink({
+      className: 'coredata',
+      description: description,
+      projectName: project
+    });
+
+  $('.actions.buttons.form').appendChild(link);
+
+});
+
+togglbutton.render('#wrapper:not(.toggl)', {observe: true}, function (elem) {
+  var link,
+    description = document.getElementsByClassName("value")[0].textContent,
+    project = $('#pagetitle a').textContent;
 
   link = togglbutton.createTimerLink({
       className: 'coredata',
